9c44aaaf0035429e28be8d2ef7e73542092fb12e
leejinwoo <leejinwoo1126@gmail.com>
2025-04-09 15:33:39 +0900

    git init

---
+++ src/main/java/com/example/simplepayment/SimplePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePaymentApplication.class, args);
+    }
+
+}
